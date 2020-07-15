5c0cb4a5bf130e82fbe0d5d9714f85f57a31b59f
HappyBoy0509 <65845452+HappyBoy0509@users.noreply.github.com>
2020-07-15 23:31:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,41 +1,33 @@
 class Translation(object):
     START_TEXT = """Hello,
-This is a Telegram File convert Bot!
-You can convert any Telegram Document, using this bot! With custom thumbnail
+Hi I Am File 2 Streamble Convert Bot!
+You can convert Any Telegram Document, Using This Bot! With Custom Thumbnail
      
     """
-    ABS_TEXT = " Please don't be selfish."
-    BANNED_USER_TEXT = "you are banned"
-    UPGRADE_TEXT = "No upgrade plan 🥰"
+    ABS_TEXT = " Please Don't Be Selfish."
+    BANNED_USER_TEXT = "You Are banned"
+    UPGRADE_TEXT = "Support HB4All"
     DOWNLOAD_START = "trying to download"
     UPLOAD_START = "trying to upload"
-    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "https://t.me/fileConvertRobot"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbot?start=anydl_bot-bot \nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
+    RCHD_BOT_API_LIMIT = "Trying To Upload."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "@HB4All_Bot"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
+    NOT_AUTH_USER_TEXT = "Support HB4ALl @HB4All_bot."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
     CUSTOM_CAPTION_UL_FILE = ""
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    CURENT_PLAN_DETAILS = """Current plan details
+    CURENT_PLAN_DETAILS = """
 --------
 Telegram ID: <code>{}</code>
-Plan name: <a href='https://t.me/InFoTelGroup/174'>{}</a>
-Expires on: {}"""
+Plan name: HB4All Plan
+Expires on: Until My Death"""
     HELP_USER = """I can do:
-👉 <a href="https://telegram.dog/FileConvertRobot">Convert To Streamable video any telegram file</a>
---------
+👉 Convert To Streamable Video Any telegram file</a>
 
-🔥 How to deploy : <a href='https://youtu.be/zQamSjXBpJU'>Click here</a>
-
-    © Group   : @InfotelGroup
-    © Channel : @DX_BotZ 
-    © credits : @Spechide
-
-Send /me to know current plan details
-
+    Send File And Reply To File /converttovideo
 """
 
-    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
+    REPLY_TO_DOC_FOR_C2V = "Reply To A Telegram Media To Convert"
