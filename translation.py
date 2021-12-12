c6a8b7622d2c664b95a0c48070b92ee053fc5944
Happy Boy <64648006+HappyBoy05@users.noreply.github.com>
2021-12-12 21:13:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -12,7 +12,7 @@ You can convert Any Telegram Document, Using This Bot! With Custom Thumbnail
     RCHD_BOT_API_LIMIT = "Trying To Upload."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "@HB4All_Bot"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \n\nUploaded in {} seconds."
-    NOT_AUTH_USER_TEXT = "Support HB4ALl @HB4All_bot."
+    NOT_AUTH_USER_TEXT = "Support @HB4All @HB4All_bot."
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
